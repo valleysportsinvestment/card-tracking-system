76417a1428d59d9543b0f52cfcba3a218ad608cd
valleysportsinvestment <valleysportsinvestment@gmail.com>
2025-07-27 15:29:57 -0700

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import NewApp from './NewApp';  // Should point to NewApp
+import App from './App';  // Back to App
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(<NewApp />);
+root.render(<App />);  // Back to App
